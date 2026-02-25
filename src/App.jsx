51d6fe217a9c93a8ea70e45fb552e83062afbbdc
V0NTIK <ocwyman@gmail.com>
2026-02-25 17:44:24 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4343,6 +4343,10 @@ const fetchCanvasTasks = async () => {
                     className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-purple-500"
                   >
                     <option value="">Select your grade...</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                    <option value="6">6</option>
                     <option value="7">7</option>
                     <option value="8">8</option>
                     <option value="9">9</option>
